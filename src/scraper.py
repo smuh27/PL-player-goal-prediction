8ab181fcc9355ec478946def294ef8dcf65d6d17
Samir Muhammad <63614993+smuh27@users.noreply.github.com>
2024-05-22 00:25:07 -0400

    All Players stats retrieved and similar player stats

--- src/scraper.py
+++ src/scraper.py
@@ -4,63 +4,9 @@ from bs4 import BeautifulSoup
 from collections import Counter
 from io import StringIO
 
-# def has_same_characters(string1, string2):
-#     # Count the frequency of characters in both strings
-#     freq1 = {}
-#     freq2 = {}
-#     for char in string1:
-#         freq1[char] = freq1.get(char, 0) + 1
-#     for char in string2:
-#         freq2[char] = freq2.get(char, 0) + 1
-    
-#     # Check if the frequencies of characters are the same in both strings
-#     return freq1 == freq2
 
-
-# url = 'https://www.bbc.com/sport/football/premier-league/top-scorers'
-# data = requests.get(url).text
-# soup = BeautifulSoup(data, 'html.parser')
-
-
-# tables = soup.find_all('table')
-# table = soup.find('table', class_='gs-o-table')
-# pattern = re.compile(r'^(.*?)\b(?:([A-Z][a-z]*)\b|$)')
-# # 'Goals', 'Played', 'GPM', 'MPG', 'Total Shots', 'Goal Conversion', 'Shot Accuracy'
-# data_list = []
-# for row in table.tbody.find_all('tr'):
-#     columns = row.find_all('td')
-#     if (columns != []):
-#         # Find the player name and team name elements
-#         player_name_element = row.find('div', class_='sp-c-top-scorers__player-name')
-#         team_name_element = row.find('div', class_='sp-c-top-scorers__teams')
-#         # Extract text content from the elements
-#         goal = columns[2].text.strip()
-#         played = columns[4].text.strip()
-#         gpm = columns[5].text.strip()
-#         mpg = columns[6].text.strip()
-#         total_shots = columns[7].text.strip()
-#         goal_conv = columns[8].text.strip()
-#         shot_acc = columns[9].text.strip()
-
-#         player_name = player_name_element.text.strip()
-#         team_name = team_name_element.text.strip()
-#         names = player_name.split()
-#         last_name = names[1::2]
-#         data_list.append({'FirstName': ''.join(names[::2]), 'LastName': ''.join(last_name), 'Goals': goal, 'Played': played, 'GPM': gpm, 'MPG': mpg, 'Total Shots': total_shots, 'Goal Conversion': goal_conv, 'Shot Accuracy': shot_acc})
-
-
-
-
-# for i in range(len(data_list)):
-#     string2 = data_list[i]['LastName']
-#     string1 = data_list[i]['FirstName']
-#     if (has_same_characters(string1, string2) == True):
-#         string2 = ''
-#     string2 = string2[len(string2)//2:]
-#     data_list[i]['LastName'] = string2
-#     df = pd.DataFrame(data_list)
 haaland_url = 'https://fbref.com/en/players/1f44ac21/Erling-Haaland'
-
+similar_players_url = {'Dovbyk' : 'https://fbref.com/en/players/5b847bb0/Artem-Dovbyk', 'Kane' : 'https://fbref.com/en/players/21a66f6a/Harry-Kane', 'Osimhen': 'https://fbref.com/en/players/8c90fd7a/Victor-Osimhen', 'Joselu' : 'https://fbref.com/en/players/6265208f/Joselu', 'Vlahovic': 'https://fbref.com/en/players/79443529/Dusan-Vlahovic' }
 
 def scrape_player_data(url):
     response = requests.get(url).text
@@ -70,10 +16,9 @@ def scrape_player_data(url):
     shoot_stats = pd.read_html(response_io, header=1)[4]
     standard_stats = pd.read_html(response_io, header=1)[3]
 
+
     last5_shoot = shoot_stats.iloc[3:9].reset_index(drop=True)
     last5_standard = standard_stats.iloc[3:9].reset_index(drop=True)
-
-
     shooting_columns_to_keep = ['Sh', 'SoT', 'SoT%', 'Sh/90', 'SoT/90', 'G/Sh', 'G/SoT']
     standard_columns_to_keep = ['Season', 'Age', 'Squad', 'Country', 'Comp', '90s', 'LgRank', 'MP', 'Starts', 'Min', 'Gls', 'xG']
 
@@ -88,18 +33,23 @@ def scrape_player_data(url):
 
     standard_list['Gls/90'] = standard_list['Gls'] / standard_list['90s']
 
+    addition_shooting_stats = last5_shoot['Dist']
 
-    data_list = pd.concat([standard_list, shooting_list], axis=1)
+    data_list = pd.concat([standard_list, shooting_list, addition_shooting_stats], axis=1)
 
     return data_list
 
-def scrape_similar_player_data():
-    response = requests.get(url).text
-    response_io = StringIO(response)
     
+    
+
+
 
 
 if __name__ == "__main__":
     data = scrape_player_data(haaland_url)
-    data.to_csv('../data/processed/haaland_stats.csv', index=False)
+    for player_name, player_url in similar_players_url.items():
+        similar_data = scrape_player_data(player_url)
+        similar_data.to_csv(f'../data/processed/{player_name}_stats.csv', index=False)
+    
+    data.to_csv('../data/processed/Haaland_stats.csv', index=False)
     print("Data scraped and saved to 'data/processed_data.csv'.")
--- src/train_model.py
+++ src/train_model.py
@@ -1,19 +1,46 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
+import numpy as np
 
-data_path = '../data/processed/haaland_stats.csv'
-data = pd.read_csv(data_path)
-feature_cols = ['Gls', 'Gls/90', 'Sh', 'SoT', 'SoT%', 'Sh/90', 'SoT/90', 'G/Sh', 'G/SoT']
-target_col = 'xG'
+def load_data(file_path):
+    return pd.read_csv(file_path)
 
-X = data[feature_cols]
-Y = data[target_col]
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+if __name__ == "__main__":
+    all_player_path = '../data/processed/player_shooting_2023_2024.csv'
+    all_player_data = load_data(all_player_path)
+    haaland_data_path = '../data/processed/Haaland_stats.csv'
+    haaland_data = load_data(haaland_data_path)
 
-print(f"Training set size: {X_train.shape[0]}")
-print(f"Testing set size: {X_test.shape[0]}")
+    similar_players_path = [
+        '../data/processed/Dovbyk_stats.csv',
+        '../data/processed/Joselu_stats.csv',
+        '../data/processed/Kane_stats.csv',
+        '../data/processed/Osimhen_stats.csv',
+        '../data/processed/Vlahovic_stats.csv'
+    ]
+
+    similar_players_data = pd.concat([load_data(file) for file in similar_players_path], ignore_index=True)
+    combined_data = pd.concat([haaland_data, similar_players_data, all_player_data], ignore_index=True)
+
+
+    feature_cols = ['Gls', 'Gls/90', 'Sh', 'SoT', 'SoT%', 'Sh/90', 'SoT/90', 'G/Sh', 'G/SoT']
+    target_col = 'xG'
+
+    X = combined_data[feature_cols]
+    Y = combined_data[target_col]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+    print(f"Training set size: {X_train.shape[0]}")
+    print(f"Testing set size: {X_test.shape[0]}")
+    
+    model = LinearRegression()
+
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    mse_scores = cross_val_score(model, X_train, y_train, scoring='neg_mean_squared_error', cv=kf)
+    r2_scores = cross_val_score(model, X_train, y_train, scoring='r2', cv=kf)
 
